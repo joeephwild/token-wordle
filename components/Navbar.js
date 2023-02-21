99e9cabd0063389b7b79bbd1818b1675ac9b1c76
Otaiki1 <otaikisadiq2804@gmail.com>
2023-02-21 14:46:06 +0100

    feat: add landing Nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,16 @@
 export default function Navbar(){
 
     return(
-        <nav className="bg-black w-full text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repudiandae dolorem facilis, nemo dolor iste placeat accusantium ipsam veniam! Enim minima nobis voluptas ducimus? Vitae laboriosam quasi perferendis veritatis hic.
+        <nav className="bg-black w-full text-white px-32">
+            <div className="flex justify-between items-center">
+                <div>
+                    <img src="./images/Logo.png" alt="" />
+                </div>
+                <div className="">
+                    <a href="" className="px-8 py-4 hover:border">Login</a>
+                    <a href="" className="px-8 py-4 hover:border">Register</a>
+                </div>
+            </div>
         </nav>
     )
 }
