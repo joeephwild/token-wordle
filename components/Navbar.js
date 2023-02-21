f147f7a793f2640b3df1df11996640bc7dab4c9c
Otaiki1 <otaikisadiq2804@gmail.com>
2023-02-21 14:39:56 +0100

    feat: setup tailwind config

---
+++ components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar(){
+
+    return(
+        <nav className="bg-black w-full text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repudiandae dolorem facilis, nemo dolor iste placeat accusantium ipsam veniam! Enim minima nobis voluptas ducimus? Vitae laboriosam quasi perferendis veritatis hic.
+        </nav>
+    )
+}
