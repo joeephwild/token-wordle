fccd211535c07ee1ef0f85ee1783f08c8572322b
Otaiki1 <otaikisadiq2804@gmail.com>
2023-02-21 16:46:17 +0100

    refac: fix responsivity

--- components/LogReg1.js
+++ components/LogReg1.js
@@ -1,13 +1,13 @@
 export default function LogReg1(){
     return(
         <section className="py-5 mt-24">
-            <div className="w-[80%] mx-auto">
+            <div className="w-full md:w-[80%] mx-auto">
                 <img src="./images/LandingImage.png" alt="" className="w-[80%] block mx-auto"/>
             </div> 
-            <div className="flex w-[80%] mx-auto justify-around items-center mt-16">
-                <img src="./images/Line.png" alt="" className="block h-1/2"/>
-                <a href="" className="block bg-black rounded-md text-2xl font-bold text-white w-48 text-center py-5">Get Started</a>
-                <img src="./images/Line.png" alt="" className="block h-1/2"/>
+            <div className="md:flex w-[80%] mx-auto justify-around items-center mt-16">
+                <img src="./images/Line.png" alt="" className="h-1/2"/>
+                <a href="" className="block bg-black rounded-md text-2xl font-bold text-white w-full md:w-48 text-center py-5">Get Started</a>
+                <img src="./images/Line.png" alt="" className="h-1/2"/>
             </div>
         </section>
     )
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,21 @@
-export default function Navbar(){
+import { useState } from "react"
 
+export default function Navbar(){
+    const [showMenu, setShowMenu] = useState(false)
     return(
-        <nav className="bg-black w-full text-white px-32">
+        <nav className="bg-black w-full text-white md:px-32 px-3">
             <div className="flex justify-between items-center">
                 <div>
-                    <img src="./images/Logo.png" alt="" />
+                    <img src="./images/Logo.png" alt="" className="w-24"/>
                 </div>
-                <div className="">
-                    <a href="" className="px-8 py-4 hover:border">Login</a>
-                    <a href="" className="px-8 py-4 hover:border">Register</a>
+                <div class="md:hidden block" id="burger" onClick={() => setShowMenu(!showMenu)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-12">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>                  
+                </div>
+                <div className={showMenu ? "block" :"hidden md:flex"}>
+                    <a href="" className="block px-8 py-4 hover:border">Login</a>
+                    <a href="" className="block px-8 py-4 hover:border">Register</a>
                 </div>
             </div>
         </nav>
