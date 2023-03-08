3d2f68703d173e3903fa0c2b3aa30a9ba004ab3a
Otaiki1 <otaikisadiq2804@gmail.com>
2023-03-08 16:47:49 +0100

    refac: optimize game contract

--- contracts/GameContract.sol
+++ contracts/GameContract.sol
@@ -1,47 +1,65 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+// This is a Solidity smart contract for a simple game where players can play and win rewards
+// The contract uses encryption to verify the data provided by the players
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract EncryptionContract {
+    // Declare errors for player and winner verification
     error Error__NotPlayed();
     error Error__NotWon();
-
+    // Declare private immutable variables to store secret keys
     bytes32 private immutable playedSecretKey;
     bytes32 private immutable wonSecretKey;
 
+    // Constructor to initialize the secret keys
     constructor(bytes32 _playedSecretKey, bytes32 _wonSecretKey) {
         playedSecretKey = _playedSecretKey;
         wonSecretKey = _wonSecretKey;
     }
 
-    function checkData(string memory encryptedData) public view returns (bool) {
+    // Function to check if encrypted data is decrypted correctly for players
+    function checkData(bytes memory encryptedData) public view returns (bool) {
+        // Ensure that encrypted data is not empty
+        require(encryptedData.length > 0, "Encrypted data must not be empty");
+
+        // Decrypt the encrypted data using the playedSecretKey
         string memory decryptedBytes = _decryptData(encryptedData);
+
+        // If decryption fails, revert with an error message
+        if (bytes(decryptedBytes).length == 0) {
+            revert("Failed to decrypt data");
+        }
+
+        // Return true if decrypted data is correct, false otherwise
         return _isDataDecryptedCorrectly(encryptedData, decryptedBytes);
     }
 
-    function _decryptData(string memory encryptedData)
+    // Function to decrypt encrypted data using the playedSecretKey
+    function _decryptData(bytes memory encryptedData)
         private
         view
         returns (string memory)
     {
-        bytes memory encryptedBytes = bytes(encryptedData);
-        bytes memory decryptedBytes = new bytes(encryptedBytes.length);
+        bytes memory decryptedBytes = new bytes(encryptedData.length);
 
-        for (uint256 i = 0; i < encryptedBytes.length; i++) {
+        for (uint256 i = 0; i < encryptedData.length; i++) {
             decryptedBytes[i] =
-                encryptedBytes[i] ^
+                encryptedData[i] ^
                 playedSecretKey[i % playedSecretKey.length];
         }
 
         return string(decryptedBytes);
     }
 
+    // Function to check if decrypted data is correct for players
     function _isDataDecryptedCorrectly(
-        string memory encryptedData,
+        bytes memory encryptedData,
         string memory decryptedData
     ) private view returns (bool) {
-        bytes memory encryptedBytes = bytes(encryptedData);
+        bytes memory encryptedBytes = encryptedData;
         bytes memory decryptedBytes = bytes(decryptedData);
 
         // Check that the length of the decrypted data is the same as the length of the encrypted data
@@ -49,7 +67,7 @@ contract EncryptionContract {
             return false;
         }
 
-        // Decrypt the encrypted data using the secret key
+        // Decrypt the encrypted data using the playedSecretKey
         for (uint256 i = 0; i < encryptedBytes.length; i++) {
             decryptedBytes[i] =
                 encryptedBytes[i] ^
@@ -65,26 +83,26 @@ contract EncryptionContract {
     }
 
     //FOR WINNERS CHECKS
-    function checkDataW(string memory encryptedData)
-        public
-        view
-        returns (bool)
-    {
-        string memory decryptedBytes = _decryptDataW(encryptedData);
-        return _isDataDecryptedCorrectlyW(encryptedData, decryptedBytes);
+    function checkDataW(bytes memory encryptedData) public view returns (bool) {
+        require(encryptedData.length > 0, "Encrypted data must not be empty");
+
+        string memory decryptedBytes = _decryptData(encryptedData);
+        if (bytes(decryptedBytes).length == 0) {
+            revert("Failed to decrypt data");
+        }
+        return _isDataDecryptedCorrectly(encryptedData, decryptedBytes);
     }
 
-    function _decryptDataW(string memory encryptedData)
+    function _decryptDataW(bytes memory encryptedData)
         private
         view
         returns (string memory)
     {
-        bytes memory encryptedBytes = bytes(encryptedData);
-        bytes memory decryptedBytes = new bytes(encryptedBytes.length);
+        bytes memory decryptedBytes = new bytes(encryptedData.length);
 
-        for (uint256 i = 0; i < encryptedBytes.length; i++) {
+        for (uint256 i = 0; i < encryptedData.length; i++) {
             decryptedBytes[i] =
-                encryptedBytes[i] ^
+                encryptedData[i] ^
                 wonSecretKey[i % wonSecretKey.length];
         }
 
@@ -92,10 +110,10 @@ contract EncryptionContract {
     }
 
     function _isDataDecryptedCorrectlyW(
-        string memory encryptedData,
+        bytes memory encryptedData,
         string memory decryptedData
     ) private view returns (bool) {
-        bytes memory encryptedBytes = bytes(encryptedData);
+        bytes memory encryptedBytes = encryptedData;
         bytes memory decryptedBytes = bytes(decryptedData);
 
         // Check that the length of the decrypted data is the same as the length of the encrypted data
@@ -136,14 +154,14 @@ contract GameContract is EncryptionContract {
     //mapping that stores winners
     mapping(address => uint256) public winners;
 
-    function playedGame(string memory _encryptedData) public {
+    function playedGame(bytes memory _encryptedData) public {
         if (!(checkData(_encryptedData))) {
             revert Error__NotPlayed();
         }
         s_stakingToken.transfer(msg.sender, REWARD_AMOUNT);
     }
 
-    function wonGame(string memory _encryptedData) public {
+    function wonGame(bytes memory _encryptedData) public {
         if ((checkDataW(_encryptedData))) {
             winners[msg.sender] += 1;
         } else {
@@ -151,9 +169,7 @@ contract GameContract is EncryptionContract {
         }
     }
 
-    function updateWinners(address _sender,uint _amount) external{
+    function updateWinners(address _sender, uint256 _amount) external {
         winners[_sender] = _amount;
     }
-
-
 }
